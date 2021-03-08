149cdcb29a44092afeff5ebf9772517dc84404b5
Daniela Zavala <danny15zavalalic@gmail.com>
2021-03-08 12:04:47 -0600

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AgmCoreModule } from '@agm/core';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyDj6YLOn08372zGvw4l01tFCs20jToN8XE'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
